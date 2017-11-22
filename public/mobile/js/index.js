45d7ec08ab75d78d7554dd5c89955ff19295c7c9
feichong <370635054@qq.com>
2017-11-22 21:44:20 +0800

    首页和搜索中心布局完成

---
+++ public/mobile/js/index.js
@@ -0,0 +1,12 @@
+$(function () {
+    // 回弹
+    mui('.mui-scroll-wrapper').scroll({
+        deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+    });
+
+
+    // 轮播图
+    mui('.mui-slider').slider({
+        interval:1000//自动轮播周期，若为0则不自动播放，默认为0；
+    });
+})
